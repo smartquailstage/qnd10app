0b162c25cac67f0d88d0f83505e140a8fb431707
smartquailstage <smartquail.stage@gmail.com>
2024-04-02 13:34:38 -0500

    update

--- qnd10app/qnd10app/settings/stage.py
+++ qnd10app/qnd10app/settings/stage.py
@@ -1,35 +1,20 @@
 from .base import *
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 ENV_FILE_PATH = BASE_DIR / ".env_stage"
 load_dotenv(str(ENV_FILE_PATH))
 
+DEBUG=  "0"
 
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = os.environ.get("DEBUG")
-ALLOWED_HOSTS = [os.environ.get("ENV_ALLOWED_HOST")]
-
-WAGTAILADMIN_BASE_URL = os.environ.get("WAGTAILADMIN_BASE_URL")  
-#Nombre del sitio web
-WAGTAIL_SITE_NAME =os.environ.get("WAGTAIL_SITE_NAME")  
-
-WAGTAILIMAGES_MAX_UPLOAD_SIZE = 30 * 1024 * 1024   # 15mb
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-ROOT_URLCONF = 'qnd10app.urls'
 
 
 BATON = {
-    'SITE_HEADER': '<a href="#"><img src="https://qnd41appk8s-bucket.nyc3.digitaloceanspaces.com/static/img/m2.png" height="26px"></a>',
+    'SITE_HEADER': '<a href="#"><img src="/static/img/m2.png" height="26px"></a>',
     'SITE_TITLE': '',
     'INDEX_TITLE': 'BUSINESS ANALITYCS PLATFORM ',
     'SUPPORT_HREF': '#',
-    'COPYRIGHT': '<a href="#"><img src="https://qnd41appk8s-bucket.nyc3.digitaloceanspaces.com/static/img/m2.png" height="18px"></a>&nbsp;&nbsp; copyright © 2022', # noqa
-    'POWERED_BY': '<a href="#"><img src="https://qnd41appk8s-bucket.nyc3.digitaloceanspaces.com/static/img/logo_smartquailgray.png" height="13px"</a>',
+    'COPYRIGHT': '<a href="#"><img src="/static/img/m2.png" height="18px"></a>&nbsp;&nbsp; copyright © 2022', # noqa
+    'POWERED_BY': '<a href="#"><img src="/static/img/logo_smartquailgray.png" height="13px"</a>',
     'CONFIRM_UNSAVED_CHANGES': True,
     'SHOW_MULTIPART_UPLOADING': True,
     'ENABLE_IMAGES_PREVIEW': True,
@@ -40,7 +25,7 @@ BATON = {
     'MENU_TITLE': 'Todo en Orden',
     'MESSAGES_TOASTS': False,
     'GRAVATAR_DEFAULT_IMG': 'retro',
-    'LOGIN_SPLASH': 'https://qnd41appk8s-bucket.nyc3.digitaloceanspaces.com/static/img/login_splash.jpg',
+    'LOGIN_SPLASH': '/static/img/login_splash.jpg',
     'SEARCH_FIELD': {
         'label': 'Search contents...',
          'url': '/search/',
@@ -246,9 +231,6 @@ BATON = {
 }
 
 
-
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -281,40 +263,13 @@ if DB_IS_AVIAL and POSTGRES_READY:
         "PORT": DB_PORT,
     }
 }
-    
-#SITE_ID = 1
 
-
-REDIS_HOST=os.environ.get('REDIS_HOST')
-REDIS_PORT=os.environ.get('REDIS_PORT')
-REDIS_DB =os.environ.get('REDIS_DB')  
-
-
-#Email setups
-EMAIL_HOST          = os.environ.get('EMAIL_HOST')
-EMAIL_PORT          =  os.environ.get('EMAIL_PORT')
-EMAIL_HOST_USER     = os.environ.get('EMAIL_HOST_USER ')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL  = EMAIL_HOST_USER
-EMAIL_BACKEND       = os.environ.get('EMAIL_BACKEND')
-EMAIL_USE_TLS       = True
-EMAIL_USE_SSL       = False
-
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "static"
-
-STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles"
-]
+#Static files DevMod
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
-MEDIAFILES_DIRS = [
-    BASE_DIR / "mediafiles"
-]
+MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [BASE_DIR / "staticfiles"]  
+STATIC_URL = "/static/"
+STATIC_ROOT = STATIC_ROOT = BASE_DIR / "static"
 
-
-
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
