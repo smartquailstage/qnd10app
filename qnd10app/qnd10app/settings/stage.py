92cfcad24acfda2cef961101c710357915dae1f4
smartquailstage <smartquail.stage@gmail.com>
2024-04-22 17:49:04 -0500

    update

--- qnd10app/qnd10app/settings/stage.py
+++ qnd10app/qnd10app/settings/stage.py
@@ -6,9 +6,15 @@ load_dotenv(str(ENV_FILE_PATH))
 
 DEBUG=True
 
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+#DEBUG = os.environ.get("DEBUG")
+#ALLOWED_HOSTS = [os.environ.get("ENV_ALLOWED_HOST")]
+
 CSRF_COOKIE_DOMAIN=".smartquail.io"
 CSRF_COOKIE_SECURE = True
-CSRF_TRUSTED_ORIGINS = ['https://quitocultura.smartquail.io']
+CSRF_TRUSTED_ORIGINS = ['https://quitocultura.smartquail.io', 'http://127.0.0.1']
 
 
 BATON = {
